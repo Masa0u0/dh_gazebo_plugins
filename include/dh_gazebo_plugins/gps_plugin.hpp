8bc444c7dd0f511a8b65a2624e2a9a0772ccfcb1
Masa0u0 <masa0u0masa1215@gmail.com>
2023-03-09 18:50:04 +0900

    first commit

---
+++ include/dh_gazebo_plugins/gps_plugin.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <random>
+#include <ros/ros.h>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/sensors/sensors.hh>
+#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/Vector3WithCovarianceStamped.h>
+
+namespace gazebo
+{
+// Default values
+static constexpr char defaultGpsTopic[] = "gps";
+static constexpr char defaultGroundSpeedTopic[] = "ground_speed";
+static constexpr double defaultHorPosStdDev = 3.;
+static constexpr double defaultVerPosStdDev = 6.;
+static constexpr double defaultHorVelStdDev = 0.1;
+static constexpr double defaultVerVelStdDev = 0.1;
+
+class GazeboGpsPlugin : public SensorPlugin
+{
+public:
+  using NormalDistribution = std::normal_distribution<double>;
+  using PosTopic = sensor_msgs::NavSatFix;
+  using VelTopic = geometry_msgs::Vector3WithCovarianceStamped;
+
+  GazeboGpsPlugin();
+  virtual ~GazeboGpsPlugin();
+
+protected:
+  void Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf);
+
+  void onUpdate();
+
+private:
+  ros::NodeHandle nh_;
+
+  ros::Publisher gz_gps_pub_;
+  ros::Publisher gz_ground_speed_pub_;
+
+  std::string ns_;                  // Namespace, read from SDF file.
+  std::string gps_topic_;           // Name of topic for GPS messages, read from SDF file.
+  std::string ground_speed_topic_;  // Name of topic for ground speed messages, read from SDF file.
+
+  sensors::GpsSensorPtr parent_sensor_;    // Pointer to the parent sensor
+  physics::WorldPtr world_;                // Pointer to the world.
+  physics::LinkPtr link_;                  // Pointer to the sensor link.
+  event::ConnectionPtr updateConnection_;  // Pointer to the update event connection.
+
+  PosTopic gz_gps_message_;           // GPS message to be published on sensor update.
+  VelTopic gz_ground_speed_message_;  // Ground speed message to be published on sensor update.
+
+  NormalDistribution ground_speed_n_[3];  // Normal distributions for ground speed noise.
+  std::random_device random_device_;      // Random device.
+  std::mt19937 random_generator_;         // Random number generator.
+};
+}  // namespace gazebo
---
+++ src/dh_gazebo_plugins/gps_plugin.cpp
@@ -0,0 +1,152 @@
+#include <sensor_msgs/NavSatStatus.h>
+
+#include "../../include/dh_gazebo_plugins/gps_plugin.hpp"
+#include "../../include/dh_gazebo_plugins/utils.hpp"
+
+using namespace std;
+
+namespace gazebo
+{
+GazeboGpsPlugin::GazeboGpsPlugin() : SensorPlugin(), random_generator_(random_device_())
+{
+}
+
+GazeboGpsPlugin::~GazeboGpsPlugin()
+{
+}
+
+void GazeboGpsPlugin::Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf)
+{
+  // Store the pointer to the parent sensor.
+  parent_sensor_ = dynamic_pointer_cast<sensors::GpsSensor>(sensor);
+  world_ = physics::get_world(parent_sensor_->WorldName());
+
+  //==============================================//
+  //========== READ IN PARAMS FROM SDF ===========//
+  //==============================================//
+
+  string link_name;
+
+  if (sdf->HasElement("robotNamespace"))
+  {
+    ns_ = sdf->GetElement("robotNamespace")->Get<string>();
+  }
+  else
+  {
+    gzerr << "[gazebo_gps_plugin] Please specify a robotNamespace.\n";
+  }
+
+  if (sdf->HasElement("linkName"))
+  {
+    link_name = sdf->GetElement("linkName")->Get<string>();
+  }
+  else
+  {
+    gzerr << "[gazebo_gps_plugin] Please specify a linkName.\n";
+  }
+
+  // Get the pointer to the link that holds the sensor.
+  link_ = boost::dynamic_pointer_cast<physics::Link>(world_->EntityByName(link_name));
+  if (link_ == NULL)
+  {
+    gzerr << "[gazebo_gps_plugin] Couldn't find specified link \"" << link_name << "\"\n";
+  }
+
+  getSdfParam<string>(sdf, "gpsTopic", gps_topic_, defaultGpsTopic);
+  getSdfParam<string>(sdf, "groundSpeedTopic", ground_speed_topic_, defaultGroundSpeedTopic);
+
+  double hor_pos_std_dev;
+  double ver_pos_std_dev;
+  double hor_vel_std_dev;
+  double ver_vel_std_dev;
+  getSdfParam<double>(sdf, "horPosStdDev", hor_pos_std_dev, defaultHorPosStdDev);
+  getSdfParam<double>(sdf, "verPosStdDev", ver_pos_std_dev, defaultVerPosStdDev);
+  getSdfParam<double>(sdf, "horVelStdDev", hor_vel_std_dev, defaultHorVelStdDev);
+  getSdfParam<double>(sdf, "verVelStdDev", ver_vel_std_dev, defaultVerVelStdDev);
+
+  // Connect to the sensor update event.
+  this->updateConnection_ =
+    this->parent_sensor_->ConnectUpdated(boost::bind(&GazeboGpsPlugin::onUpdate, this));
+
+  // Make sure the parent sensor is active.
+  parent_sensor_->SetActive(true);
+
+  // Initialize the normal distributions for ground speed.
+  ground_speed_n_[0] = NormalDistribution(0, hor_vel_std_dev);
+  ground_speed_n_[1] = NormalDistribution(0, hor_vel_std_dev);
+  ground_speed_n_[2] = NormalDistribution(0, ver_vel_std_dev);
+
+  // ============================================ //
+  // ======= POPULATE STATIC PARTS OF MSGS ====== //
+  // ============================================ //
+
+  // Fill the static parts of the GPS message.
+  gz_gps_message_.header.frame_id = link_name;
+  gz_gps_message_.status.service = sensor_msgs::NavSatStatus::SERVICE_GPS;
+  gz_gps_message_.status.status = sensor_msgs::NavSatStatus::STATUS_FIX;
+  gz_gps_message_.position_covariance_type = PosTopic::COVARIANCE_TYPE_KNOWN;
+
+  gz_gps_message_.position_covariance[0] = sqr(hor_pos_std_dev);
+  gz_gps_message_.position_covariance[1] = 0.;
+  gz_gps_message_.position_covariance[2] = 0.;
+  gz_gps_message_.position_covariance[3] = 0.;
+  gz_gps_message_.position_covariance[4] = sqr(hor_pos_std_dev);
+  gz_gps_message_.position_covariance[5] = 0.;
+  gz_gps_message_.position_covariance[6] = 0.;
+  gz_gps_message_.position_covariance[7] = 0.;
+  gz_gps_message_.position_covariance[8] = sqr(ver_pos_std_dev);
+
+  // Fill the static parts of the ground speed message.
+  gz_ground_speed_message_.header.frame_id = link_name;
+
+  gz_ground_speed_message_.vector.covariance[0] = sqr(hor_vel_std_dev);
+  gz_ground_speed_message_.vector.covariance[1] = 0.;
+  gz_ground_speed_message_.vector.covariance[2] = 0.;
+  gz_ground_speed_message_.vector.covariance[3] = 0.;
+  gz_ground_speed_message_.vector.covariance[4] = sqr(hor_vel_std_dev);
+  gz_ground_speed_message_.vector.covariance[5] = 0.;
+  gz_ground_speed_message_.vector.covariance[6] = 0.;
+  gz_ground_speed_message_.vector.covariance[7] = 0.;
+  gz_ground_speed_message_.vector.covariance[8] = sqr(ver_vel_std_dev);
+
+  // Advertise publishers
+  gz_gps_pub_ = nh_.advertise<PosTopic>("/" + ns_ + "/" + gps_topic_, 1);
+  gz_ground_speed_pub_ = nh_.advertise<VelTopic>("/" + ns_ + "/" + ground_speed_topic_, 1);
+}
+
+void GazeboGpsPlugin::onUpdate()
+{
+  // Get the time of the last measurement.
+  common::Time current_time = parent_sensor_->LastMeasurementTime();
+
+  // Get the linear velocity in the world frame.
+  ignition::math::Vector3d W_ground_speed_W_L = link_->WorldLinearVel();
+
+  // Apply noise to ground speed.
+  W_ground_speed_W_L += ignition::math::Vector3d(
+    ground_speed_n_[0](random_generator_), ground_speed_n_[1](random_generator_),
+    ground_speed_n_[2](random_generator_));
+
+  // Fill the GPS message.
+  gz_gps_message_.latitude = parent_sensor_->Latitude().Degree();
+  gz_gps_message_.longitude = parent_sensor_->Longitude().Degree();
+  gz_gps_message_.altitude = parent_sensor_->Altitude();
+  gz_gps_message_.header.stamp.sec = current_time.sec;
+  gz_gps_message_.header.stamp.nsec = current_time.nsec;
+
+  // Fill the ground speed message.
+  gz_ground_speed_message_.vector.vector.x = W_ground_speed_W_L.X();
+  gz_ground_speed_message_.vector.vector.y = W_ground_speed_W_L.Y();
+  gz_ground_speed_message_.vector.vector.z = W_ground_speed_W_L.Z();
+  gz_ground_speed_message_.header.stamp.sec = current_time.sec;
+  gz_ground_speed_message_.header.stamp.nsec = current_time.nsec;
+
+  // Publish the GPS message.
+  gz_gps_pub_.publish(gz_gps_message_);
+
+  // Publish the ground speed message.
+  gz_ground_speed_pub_.publish(gz_ground_speed_message_);
+}
+
+GZ_REGISTER_SENSOR_PLUGIN(GazeboGpsPlugin);
+}  // namespace gazebo
